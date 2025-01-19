540518f7ef0a834b59443e00552b133efe79d5a9
Anubis-vc <114633659+Anubis-vc@users.noreply.github.com>
2025-01-19 00:11:22 -0500

    added header animations, completed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,62 +1,98 @@
+'use client';
 import React from 'react'
 import { SocialIcon } from 'react-social-icons';
+import { motion } from "framer-motion";
 
 type Props = {}
 
-export default function Header({}: Props) {
-  return (
-	<header className='sticky flex items-center justify-center top-0 bg-slateBlue h-16'>
-		<div className='flex justify-between max-w-7xl mx-auto z-20 w-full px-4'>
-			<div className="flex flex-row items-center gap-2">
-				{/* Social Icons */}
-				<SocialIcon 
-					target="_blank"
-					className='hover:scale-105 transition duration-200 ease-in-out'
-					url="https://twitter.com/notved"
-					fgColor="black" 
-					bgColor="transparent"
-				/>
-				<SocialIcon
-					target="_blank"
-					className='hover:scale-105 transition duration-200 ease-in-out'
-					url="https://www.instagram.com/ved.dchugh/"
-					fgColor="black"
-					bgColor="transparent"
-				/>
-				<SocialIcon
-					target="_blank"
-					className='hover:scale-105 transition duration-200 ease-in-out'
-					url="https://www.linkedin.com/in/vedchugh"
-					fgColor="black"
-					bgColor="transparent"
-				/>
-				<SocialIcon
-					target="_blank"
-					className='hover:scale-105 transition duration-200 ease-in-out'
-					url="https://github.com/Anubis-vc"
-					fgColor="black"
-					bgColor="transparent"
-				/>
-				<SocialIcon
-					target="_blank"
-					className='hover:scale-105 transition duration-200 ease-in-out'
-					url="https://letterboxd.com/vedchugh/"
-					fgColor="black"
-					bgColor="transparent"
-				/>
-			</div>
+export default function Header({ }: Props) {
+	return (
+		<header className='sticky flex items-center justify-center top-0 bg-slateBlue h-16'>
+			<div className='flex justify-between max-w-7xl mx-auto z-20 w-full p-5 xl:items-center'>
+				<motion.div
+					initial={{
+						x: -500,
+						opacity: 0,
+						scale: 0.5,
+					}}
+					animate={{
+						x: 0,
+						opacity: 1,
+						scale: 1,
+					}}
+					transition={{
+						duration: 1.5,
+						delay: 0,
+					}}
+					className="flex flex-row items-center gap-2"
+				>
+					{/* Social Icons */}
+					<SocialIcon
+						target="_blank"
+						className='hover:scale-105 transition duration-200 ease-in-out'
+						url="https://twitter.com/notved"
+						fgColor="black"
+						bgColor="transparent"
+					/>
+					<SocialIcon
+						target="_blank"
+						className='hover:scale-105 transition duration-200 ease-in-out'
+						url="https://www.instagram.com/ved.dchugh/"
+						fgColor="black"
+						bgColor="transparent"
+					/>
+					<SocialIcon
+						target="_blank"
+						className='hover:scale-105 transition duration-200 ease-in-out'
+						url="https://www.linkedin.com/in/vedchugh"
+						fgColor="black"
+						bgColor="transparent"
+					/>
+					<SocialIcon
+						target="_blank"
+						className='hover:scale-105 transition duration-200 ease-in-out'
+						url="https://github.com/Anubis-vc"
+						fgColor="black"
+						bgColor="transparent"
+					/>
+					<SocialIcon
+						target="_blank"
+						className='hover:scale-105 transition duration-200 ease-in-out'
+						url="https://letterboxd.com/vedchugh/"
+						fgColor="black"
+						bgColor="transparent"
+					/>
+				</motion.div>
 
-			<div className='flex flex-row items-center text-black-300 cursor-pointer mr-2.5
-				hover:scale-105 transition duration-200 ease-in-out'>
-				<SocialIcon
-					target="_blank"
-					network="email"
-					fgColor="black"
-					bgColor="transparent"
-				/>
-				<p className="uppercase hidden md:inline-flex text-sm text-black-400">Contact Me</p>
+				<motion.div
+					initial={{
+						x: 500,
+						opacity: 0,
+						scale: 0.5,
+					}}
+					animate={{
+						x: 0,
+						opacity: 1,
+						scale: 1,
+					}}
+					transition={{
+						duration: 1.5,
+					}}
+				>
+					<div className='flex flex-row items-center cursor-pointer
+						hover:scale-105 transition duration-200 ease-in-out'
+					>
+						<SocialIcon
+						target="_blank"
+						network="email"
+						fgColor="black"
+						bgColor="transparent"
+						/>
+						<p className="uppercase hidden md:inline-flex text-sm text-black-400">Contact Me</p>
+					</div>
+					
+				</motion.div>
 			</div>
-		</div>
-	</header>
-  );
+		</header>
+	);
 }
