ea2840cd77619fe8538e24ded4453af100445ac4
Anubis-vc <114633659+Anubis-vc@users.noreply.github.com>
2025-01-20 11:18:51 -0500

    adjusted header styles and transition

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,62 +6,97 @@ import { motion } from "motion/react";
 type Props = {}
 
 export default function Header({ }: Props) {
+	const iconBounceVariants = {
+		initial: {
+			y: 0
+		},
+		animate: {
+			y: [-50, 0],
+			transition: {
+				duration: 0.5,
+				ease: "easeOut"
+			}
+		}
+	};
+
+	const containerVariants = {
+		initial: {
+			opacity: 1
+		},
+		animate: {
+			opacity: 1,
+			transition: {
+				staggerChildren: 0.25
+			}
+		}
+	};
+
 	return (
 		<header className='sticky flex items-center justify-center top-0 bg-primary h-16 z-20'>
 			<div className='flex justify-between max-w-7xl mx-auto w-full p-5 xl:items-center'>
 				<motion.div
-					initial={{
-						x: -500,
-						opacity: 0,
-						scale: 0.5,
-					}}
-					animate={{
-						x: 0,
-						opacity: 1,
-						scale: 1,
-					}}
-					transition={{
-						duration: 1.5,
-						delay: 0,
+					variants={containerVariants}
+					initial="intial"
+					animate="animate"
+					transition= {{
+						type: "spring",
+    					duration: 0.5,
+    					bounce: 0.25
 					}}
 					className="flex flex-row items-center gap-3"
 				>
 					{/* Social Icons */}
-					<SocialIcon
-						target="_blank"
-						className='rounded-full hover:shadow-xl transition duration-200 ease-in-out'
-						url="https://twitter.com/notved"
-						fgColor="black"
-						bgColor="transparent"
-					/>
-					<SocialIcon
-						target="_blank"
-						className='hover:scale-105 transition duration-200 ease-in-out'
-						url="https://www.instagram.com/ved.dchugh/"
-						fgColor="black"
-						bgColor="transparent"
-					/>
-					<SocialIcon
-						target="_blank"
-						className='hover:scale-105 transition duration-200 ease-in-out'
-						url="https://www.linkedin.com/in/vedchugh"
-						fgColor="black"
-						bgColor="transparent"
-					/>
-					<SocialIcon
+
+					<motion.div variants={iconBounceVariants}>
+						<SocialIcon
+							target="_blank"
+							className='hover:scale-105 transition duration-200 ease-in-out'
+							url="https://twitter.com/notved"
+							fgColor="black"
+							bgColor="transparent"
+						/>
+					</motion.div>
+
+					<motion.div variants={iconBounceVariants}>
+						<SocialIcon
+							target="_blank"
+							className='hover:scale-105 transition duration-200 ease-in-out'
+							url="https://www.instagram.com/ved.dchugh/"
+							fgColor="black"
+							bgColor="transparent"
+						/>
+					</motion.div>
+
+					<motion.div variants={iconBounceVariants}>
+						<SocialIcon
+							target="_blank"
+							className='hover:scale-105 transition duration-200 ease-in-out'
+							url="https://www.linkedin.com/in/vedchugh"
+							fgColor="black"
+							bgColor="transparent"
+						/>
+					</motion.div>
+
+					<motion.div variants={iconBounceVariants}>
+						<SocialIcon
 						target="_blank"
 						className='hover:scale-105 transition duration-200 ease-in-out'
 						url="https://github.com/Anubis-vc"
 						fgColor="black"
 						bgColor="transparent"
-					/>
-					<SocialIcon
+						/>
+					</motion.div>
+					
+					<motion.div variants={iconBounceVariants}>
+						<SocialIcon
 						target="_blank"
 						className='hover:scale-105 transition duration-200 ease-in-out'
 						url="https://letterboxd.com/vedchugh/"
 						fgColor="black"
 						bgColor="transparent"
-					/>
+						/>
+					</motion.div>
+					
 				</motion.div>
 
 				<motion.div
